a5ea5af9ff2b7feb32442c939684844d2b0d795d
HirokiHayano <hoffnung123gec@gmail.com>
2026-02-15 14:23:40 +0900

    chore: prismaインスタンス生成

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,15 +1,37 @@
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// if (!globalForPrisma.prisma) {
+//   console.log("DATABASE_URL:", process.env.DATABASE_URL); // Add this line
+//   globalForPrisma.prisma = new PrismaClient();
+// }
+
+// prisma = globalForPrisma.prisma;
+
+// export default prisma;
+
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+
 
 let prisma: PrismaClient;
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({ connectionString })
 
 if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = new PrismaClient();
+  globalForPrisma.prisma = new PrismaClient({adapter});
 }
 
 prisma = globalForPrisma.prisma;
 
 export default prisma;
+
